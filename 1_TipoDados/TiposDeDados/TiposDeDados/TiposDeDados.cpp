cc3909e86bd54ab056627afcd29e13d678faccc7
Claudio Nunes <claudio@metasistemas.com>
2023-02-28 06:50:09 -0300

    pequenos ajustes

--- 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
+++ 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
@@ -6,72 +6,72 @@ using namespace std;
 
 int main()
 {
-    cout << "Estrutura de Dados - Fatec" << endl;
+	cout << "Estrutura de Dados - Fatec" << endl;
 
-    cout << "Tipos Numericos\n";
-    cout << "===============\n";
-    int i = 10;
-    short s = 18;
-    long l = 200000;
-    float f = 1.97F;
-    double  d = 4.73;
-    cout << "Inteiros\n";
-    cout << "  int i = " << i << " ocupa " << sizeof(i) << " bytes\n";
-    cout << "short s = " << s << " ocupa " << sizeof(s) << " bytes\n";
-    cout << "long  l = " << l << " ocupa " << sizeof(l) << " bytes\n\n";
-    cout << "Ponto Flutuante (Reais)\n";
-    cout << " float f = " << f << " ocupa " << sizeof(f) << " bytes\n";
-    cout << "double d = " << d << " ocupa " << sizeof(d) << " bytes\n";
+	cout << "Tipos Numericos\n";
+	cout << "===============\n";
+	int i = 10;
+	short s = 18;
+	long l = 200000;
+	float f = 1.97F;
+	double  d = 4.73;
+	cout << "Inteiros\n";
+	cout << "  int i = " << i << " ocupa " << sizeof(i) << " bytes\n";
+	cout << "short s = " << s << " ocupa " << sizeof(s) << " bytes\n";
+	cout << "long  l = " << l << " ocupa " << sizeof(l) << " bytes\n\n";
+	cout << "Ponto Flutuante (Reais)\n";
+	cout << " float f = " << f << " ocupa " << sizeof(f) << " bytes\n";
+	cout << "double d = " << d << " ocupa " << sizeof(d) << " bytes\n";
 
-    cout << "\nTipo logico (boolean)\n";
-    cout << "====================\n";
-    bool flFacil = false;
-    cout << "bool flFacil = " << flFacil << " ocupa " << sizeof(flFacil) << " bytes\n";
+	cout << "\nTipo logico (boolean)\n";
+	cout << "====================\n";
+	bool flFacil = false;
+	cout << "bool flFacil = " << flFacil << " ocupa " << sizeof(flFacil) << " bytes\n";
 
-    cout << "\nTipo caracter\n";
-    cout << "=============\n";
-    char letra = 'Z';
-    cout << "char letra = " << letra << " ocupa " << sizeof(letra) << " bytes\n";
+	cout << "\nTipo caracter\n";
+	cout << "=============\n";
+	char letra = 'Z';
+	cout << "char letra = " << letra << " ocupa " << sizeof(letra) << " bytes\n";
 
-    cout << "\nTipo string\n";
-    cout << "===========\n";
-    string texto ="Estrutura de Dados vai ser moleza!";
-    cout << "string texto = \"" << texto << "\" ocupa " << sizeof(texto) << " bytes\n";
+	cout << "\nTipo string\n";
+	cout << "===========\n";
+	string texto = "Estrutura de Dados vai ser moleza!";
+	cout << "string texto = \"" << texto << "\" ocupa " << sizeof(texto) << " bytes\n";
 
-    cout << "\nPonteiros\n";
-    cout << "===========\n";
-    string* ponteiro = &texto;
-    cout << "Para a variavel 'texto' do exemplo acima: string* ponteiro = &texto\nO endereco da memoria onde o valor foi armazenado (ponteiro)  = " << ponteiro <<endl;
-    cout << "O valor armazendo no local indicado pelo ponteiro e *ponteiro = \"" << *ponteiro << "\"";
+	cout << "\nPonteiros\n";
+	cout << "===========\n";
+	string* ponteiro = &texto;
+	cout << "Para a variavel 'texto' do exemplo acima: string* ponteiro = &texto\nO endereco da memoria onde o valor foi armazenado (ponteiro)  = " << ponteiro << endl;
+	cout << "O valor armazendo no local indicado pelo ponteiro e *ponteiro = \"" << *ponteiro << "\"";
 
-    cout << endl << endl;
-	
-    system("pause"); // somente no Windows
-    system("cls");
-	
-    cout << "Entrada de Dados" << endl;
+	cout << endl << endl;
+
+	cout << "Qualquer tecla para continuar...";
+	system("pause"); // somente no Windows
+	system("cls");
+
+	cout << "Entrada de Dados" << endl;
 	cout << "================" << endl;
 	string nome = "";
 	int idade = 0;
 	int dias = 0;
 	int horas = 0;
 	int minutos = 0;
-    
+
 	cout << "Digite seu nome: ";
 	cin >> nome;
-    cout << endl;
 	cout << "Digite sua idade: ";
-	cout << endl;
 	cin >> idade;
 	dias = idade * 365;
-	horas = dias * 24 ;
+	horas = dias * 24;
 	minutos = horas * 60;
 	cout << "Sua idade em dias e: " << dias << endl;
 	cout << "Sua idade em horas e: " << horas << endl;
 	cout << "Sua idade em minutos e: " << minutos << endl;
-    
+
+	cout << "Qualquer tecla para sair...";
 	system("pause"); // somente no Windows
-      
-    
+
+
 }
 
