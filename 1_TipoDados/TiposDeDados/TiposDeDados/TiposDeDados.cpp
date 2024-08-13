276340369c757e41b1405fb84a524e0781b08e49
Claudio Nunes <claudio.nunes6@fatec.sp.gov.br>
2024-08-13 11:59:30 -0300

    Alterei o programa

--- 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
+++ 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
@@ -7,9 +7,11 @@ using namespace std;
 int main()
 {
 	cout << "Estrutura de Dados - Fatec" << endl;
+	cout << "prof. Claudio Nunes" << endl;
 	
 	cout << "Tipos Numericos\n";
 	cout << "===============\n";
+	
 	int i = 10;
 	short s = 18;
 	long l = 200000;
@@ -64,7 +66,7 @@ int main()
 	cin >> idade;
 	dias = idade * 365;
 	horas = dias * 24;
-	minutos = horas * 60;
+	minutos = dias * 60;
 	cout << "Sua idade em dias e: " << dias << endl;
 	cout << "Sua idade em horas e: " << horas << endl;
 	cout << "Sua idade em minutos e: " << minutos << endl;
